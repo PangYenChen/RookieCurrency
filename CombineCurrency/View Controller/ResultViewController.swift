965f6e473220be2d2591d50caf42199f8ab46d5b
PangYen Chen <cpy4869@gmail.com>
2023-02-10 21:30:41 +0800

    [refine] custom init(coder:) (combine target)

--- CombineCurrency/View Controller/ResultViewController.swift
+++ CombineCurrency/View Controller/ResultViewController.swift
@@ -14,11 +14,31 @@ class ResultViewController: BaseResultViewController {
     // MARK: - property
     private var anyCancellableSet = Set<AnyCancellable>()
     
-    var numberOfDay: CurrentValueSubject<Int, Never>!
+    private var numberOfDay: CurrentValueSubject<Int, Never>
     
-    private var baseCurrency: CurrentValueSubject<ResponseDataModel.RateList.Currency, Never>!
+    private var baseCurrency: CurrentValueSubject<ResponseDataModel.RateList.Currency, Never>
        
     // MARK: - Method
+    required init?(coder: NSCoder) {
+        if let baseCurrencyString = UserDefaults.standard.string(forKey: "baseCurrency"),
+           let baseCurrency = ResponseDataModel.RateList.Currency.init(rawValue: baseCurrencyString) {
+            self.baseCurrency = CurrentValueSubject(baseCurrency)
+        } else {
+            let defaultCurrency = Currency.TWD
+            baseCurrency = CurrentValueSubject(.TWD)
+        }
+        
+        let userSettingNumber = UserDefaults.standard.integer(forKey: "numberOfDay")
+        if userSettingNumber > 0 {
+            numberOfDay = CurrentValueSubject(userSettingNumber)
+        } else {
+            let defaultNumber = 33
+            numberOfDay = CurrentValueSubject(defaultNumber)
+        }
+        
+        super.init(coder: coder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,17 +54,13 @@ class ResultViewController: BaseResultViewController {
             .assign(to: \.text, on: latestUpdateTimeLabel)
             .store(in: &anyCancellableSet)
         
-        // number of day
-        do {
-            let userSettingNumber = UserDefaults.standard.integer(forKey: "numberOfDay")
-            if userSettingNumber > 0 {
-                numberOfDay = CurrentValueSubject(userSettingNumber)
-                stepper.value = Double(userSettingNumber)
-            } else {
-                let defaultNumber = 33
-                numberOfDay = CurrentValueSubject.init(defaultNumber)
-                stepper.value = Double(defaultNumber)
-            }
+        
+        do { // number of day
+            numberOfDay
+                .first()
+                .map(Double.init)
+                .assign(to: \.value, on: stepper)
+                .store(in: &anyCancellableSet)
             
             numberOfDay
                 .map(String.init)
@@ -62,22 +78,12 @@ class ResultViewController: BaseResultViewController {
                 .store(in: &anyCancellableSet)
         }
         
-        // base currency
-        do {
-            if let baseCurrencyString = UserDefaults.standard.string(forKey: "baseCurrency") {
-                if let baseCurrency = ResponseDataModel.RateList.Currency.init(rawValue: baseCurrencyString) {
-                    self.baseCurrency = CurrentValueSubject(baseCurrency)
-                } else {
-                    self.baseCurrency = CurrentValueSubject(.TWD)
-                }
-            } else {
-                baseCurrency = CurrentValueSubject(.TWD)
-            }
-            
+        
+        do { // base currency
             baseCurrency
                 .dropFirst()
                 .map { baseCurrency -> String in baseCurrency.rawValue}
-                .sink { UserDefaults.standard.set($0, forKey: "baseCurrency")}
+                .sink { UserDefaults.standard.set($0, forKey: "baseCurrency") }
                 .store(in: &anyCancellableSet)
             
             baseCurrency
