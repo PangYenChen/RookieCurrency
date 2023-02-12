48c002ac1bd59b2a0f48d28f7d9cc4bfaf75119e
PangYen Chen <cpy4869@gmail.com>
2023-02-12 23:01:55 +0800

    [feature] result table view 使用 diffable data source

--- RookieCurrency/View Controller/ResultViewController.swift
+++ RookieCurrency/View Controller/ResultViewController.swift
@@ -25,6 +25,10 @@ class ResultTableViewController: UITableViewController {
     
     private var filteredAnalyzedDataArray: [(currency: Currency, latest: Double, mean: Double, deviation: Double)] = []
     
+    private var analyzedDataDictionary: [Currency: (latest: Double, mean: Double, deviation: Double)]
+    
+    private var dataSource: DataSource!
+    
     // MARK: - Methods
     required init?(coder: NSCoder) {
         
@@ -47,6 +51,9 @@ class ResultTableViewController: UITableViewController {
             searchText = String()
         }
         
+        do { // analyzed data
+            analyzedDataDictionary = [:]
+        }
         super.init(coder: coder)
         
         do { // search controller
@@ -76,6 +83,29 @@ class ResultTableViewController: UITableViewController {
             tableView.refreshControl = refreshControl
             let handler = UIAction { [unowned self] _ in refresh() }
             refreshControl.addAction(handler, for: .primaryActionTriggered)
+            
+            dataSource = DataSource(tableView: tableView) { [unowned self] tableView, indexPath, currency in
+                let reusedIdentifier = R.reuseIdentifier.currencyCell.identifier
+                let cell = tableView.dequeueReusableCell(withIdentifier: reusedIdentifier, for: indexPath)
+                
+                guard let data = analyzedDataDictionary[currency] else { return nil }
+                
+                let deviationString = NumberFormatter.localizedString(from: NSNumber(value: data.deviation), number: .decimal)
+                let meanString = NumberFormatter.localizedString(from: NSNumber(value: data.mean), number: .decimal)
+                let latestString = NumberFormatter.localizedString(from: NSNumber(value: data.latest), number: .decimal)
+                
+                cell.textLabel?.text = [currency.code, currency.localizedString, deviationString].joined(separator: ", ")
+                cell.textLabel?.adjustsFontForContentSizeCategory = true
+                cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+                cell.textLabel?.textColor = data.deviation < 0 ? .systemGreen : .systemRed
+                
+                cell.detailTextLabel?.text = R.string.localizable.currencyCellDetail(meanString, latestString)
+                cell.detailTextLabel?.adjustsFontForContentSizeCategory = true
+                cell.detailTextLabel?.font = UIFont.preferredFont(forTextStyle: .subheadline)
+                
+                return cell
+            }
+            dataSource.defaultRowAnimation = .fade
         }
         
         refresh()
@@ -97,17 +127,17 @@ class ResultTableViewController: UITableViewController {
                 }
                 
                 do { // update table view
-                    analyzedDataArray = RateListSetAnalyst
+                    analyzedDataDictionary = RateListSetAnalyst
                         .analyze(latestRateList: latestRateList,
                                  historicalRateListSet: historicalRateListSet,
                                  baseCurrency: baseCurrency)
-                        .sorted { $0.value.deviation > $1.value.deviation }
-                        .map { (currency: $0.key, latest: $0.value.latest, mean: $0.value.mean, $0.value.deviation)}
-                    
                     updateTableView()
                 }
                 
             case .failure(let error):
+                analyzedDataDictionary = [:]
+                updateTableView()
+                
                 showErrorAlert(error: error)
             }
             
@@ -136,17 +166,23 @@ class ResultTableViewController: UITableViewController {
     
     /// 更新 table view，純粹把資料填入 table view，不動資料。
     private func updateTableView() {
-        if searchText.isEmpty {
-            filteredAnalyzedDataArray = analyzedDataArray
-        } else {
-            filteredAnalyzedDataArray = analyzedDataArray
-                .filter { (currency, _, _, _) in
-                    [currency.code, currency.localizedString]
-                        .contains { text in text.lowercased().contains(searchText.lowercased()) }
+        
+        var sortedTuple = analyzedDataDictionary
+            .sorted { $0.value.deviation > $1.value.deviation }
+         
+        if !searchText.isEmpty { // filtering if needed
+            sortedTuple = sortedTuple
+                .filter { (currency,_) in
+                    [currency.code, currency.localizedString].contains { text in text.lowercased().contains(searchText.lowercased()) }
                 }
         }
         
-        tableView.reloadData()
+        let sortedCurrencies = sortedTuple.map { $0.key }
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(sortedCurrencies)
+        
+        dataSource.apply(snapshot)
     }
     
     private func showErrorAlert(error: Error) {
@@ -180,37 +216,6 @@ class ResultTableViewController: UITableViewController {
         present(alertController, animated: true)
     }
 }
-
-// MARK: - Table view data source
-extension ResultTableViewController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return filteredAnalyzedDataArray.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let reusedIdentifier = R.reuseIdentifier.currencyCell.identifier
-        let cell = tableView.dequeueReusableCell(withIdentifier: reusedIdentifier, for: indexPath)
-        
-        let data = filteredAnalyzedDataArray[indexPath.item]
-        let currency = data.currency
-        let deviationString = NumberFormatter.localizedString(from: NSNumber(value: data.deviation), number: .decimal)
-        let meanString = NumberFormatter.localizedString(from: NSNumber(value: data.mean), number: .decimal)
-        let latestString = NumberFormatter.localizedString(from: NSNumber(value: data.latest), number: .decimal)
-        
-        cell.textLabel?.text = [currency.code, currency.localizedString, deviationString].joined(separator: ", ")
-        cell.textLabel?.adjustsFontForContentSizeCategory = true
-        cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
-        cell.textLabel?.textColor = data.deviation < 0 ? .systemGreen : .systemRed
-        
-        cell.detailTextLabel?.text = R.string.localizable.currencyCellDetail(meanString, latestString)
-        cell.detailTextLabel?.adjustsFontForContentSizeCategory = true
-        cell.detailTextLabel?.font = UIFont.preferredFont(forTextStyle: .subheadline)
-        
-        return cell
-    }
-}
     
 // MARK: - Search Bar Delegate
 extension ResultTableViewController: UISearchBarDelegate {
@@ -218,6 +223,16 @@ extension ResultTableViewController: UISearchBarDelegate {
         self.searchText = searchText
         updateTableView()
     }
+}
+
+// MARK: - name space
+extension ResultTableViewController {
+    enum Section {
+        case main
+    }
+    
+    typealias DataSource = UITableViewDiffableDataSource<Section, Currency>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Currency>
 }
 
 
