5165520838f4d9b7913ca7fb83a2b7cdbaa01ec5
PangYen Chen <cpy4869@gmail.com>
2023-02-12 20:55:37 +0800

    [refine] currency table view 使用 diffable data source

--- RookieCurrency/View Controller/CurrencyTableViewController.swift
+++ RookieCurrency/View Controller/CurrencyTableViewController.swift
@@ -15,13 +15,12 @@ class CurrencyTableViewController: UITableViewController {
     
     private let currencies: [Currency]
     
-    private var filteredCurrencies: [Currency]
+    private var dataSource: DataSource!
     
     // MARK: - method
     init?(coder: NSCoder, completionHandler: @escaping (Currency) -> Void) {
         self.completionHandler = completionHandler
         currencies = Currency.allCases
-        filteredCurrencies = currencies
         
         super.init(coder: coder)
         
@@ -35,24 +34,28 @@ class CurrencyTableViewController: UITableViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-}
-
-// MARK: - Table view data source
-extension CurrencyTableViewController {
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        filteredCurrencies.count
-    }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let identifier = R.reuseIdentifier.currencyCell.identifier
-        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        cell.textLabel?.text = filteredCurrencies[indexPath.row].localizedString
-        cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
-        cell.textLabel?.adjustsFontForContentSizeCategory = true
-        
-        return cell
+        do { // table view data source
+            dataSource = DataSource(tableView: tableView) { tableView, indexPath, itemIdentifier in
+                let identifier = R.reuseIdentifier.currencyCell.identifier
+                let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+                
+                cell.textLabel?.text = itemIdentifier.localizedString
+                cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+                cell.textLabel?.adjustsFontForContentSizeCategory = true
+                
+                return cell
+            }
+            
+            var snapshot = Snapshot()
+            snapshot.appendSections([.main])
+            snapshot.appendItems(currencies)
+            
+            dataSource.apply(snapshot)
+        }
     }
 }
 
@@ -66,19 +69,33 @@ extension CurrencyTableViewController {
     }
 }
 
-// MARK: - type something
+// MARK: - Search Bar Delegate
 extension CurrencyTableViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        
         if searchText.isEmpty {
-            filteredCurrencies = currencies
+            snapshot.appendItems(currencies)
         } else {
-            filteredCurrencies = currencies
+            let filteredCurrencies = currencies
                 .filter { currency in
                     [currency.code, currency.localizedString]
                         .contains { text in text.lowercased().contains(searchText.lowercased()) }
                 }
+            snapshot.appendItems(filteredCurrencies)
         }
         
-        tableView.reloadData()
+        dataSource.apply(snapshot)
     }
+}
+
+// MARK: - name space
+extension CurrencyTableViewController {
+    enum Section {
+        case main
+    }
+    
+    typealias DataSource = UITableViewDiffableDataSource<Section, Currency>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Currency>
 }
