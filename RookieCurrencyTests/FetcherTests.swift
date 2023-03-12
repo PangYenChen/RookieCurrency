ef014a72559c54e8fe77f9960a18cecb6cc295ed
PangYen Chen <cpy4869@gmail.com>
2023-03-12 16:30:14 +0800

    [test] imperative fetcher decode fail

--- RookieCurrencyTests/FetcherTests.swift
+++ RookieCurrencyTests/FetcherTests.swift
@@ -15,12 +15,14 @@ final class FetcherTests: XCTestCase {
     
     private var stubRateSession: StubRateSession!
     
+    private let timeoutTimeInterval: TimeInterval = 3
+    
     override func setUp() {
         stubRateSession = StubRateSession()
         sut = Fetcher(rateSession: stubRateSession)
     }
     
-#warning("還要測 timeout、429、decode error")
+#warning("還要測 timeout、429")
     
     override func tearDown() {
         sut = nil
@@ -30,15 +32,17 @@ final class FetcherTests: XCTestCase {
     func testFetchLatestRate() throws {
         
         // arrange
+        let expectation = expectation(description: "should get a decoded latest rate instance")
+        
         do {
-            stubRateSession.data = TestingData.latestData
+            let data = TestingData.latestData
             
             let url = try XCTUnwrap(URL(string: "https://www.apple.com"))
-            stubRateSession.urlResponse = HTTPURLResponse(url: url,
-                                                          statusCode: 200,
-                                                          httpVersion: nil,
-                                                          headerFields: nil)
-            stubRateSession.error = nil
+            let response = HTTPURLResponse(url: url,
+                                           statusCode: 200,
+                                           httpVersion: nil,
+                                           headerFields: nil)
+            stubRateSession.outputs = [(data: data, response: response, error: nil)]
         }
         
         // action
@@ -51,24 +55,28 @@ final class FetcherTests: XCTestCase {
                     
                     let dummyCurrency = Currency.TWD
                     XCTAssertNotNil(rate[dummyCurrency])
+                    expectation.fulfill()
                 case .failure:
-                    XCTFail()
+                    XCTFail("should get a decoded latest rate instance")
                 }
             }
+        
+        waitForExpectations(timeout: timeoutTimeInterval)
     }
     
     func testFetchHistoricalRate() throws {
-        
         // arrange
+        let expectation = expectation(description: "should get a decoded historical rate instance")
+        
         do {
-            stubRateSession.data = TestingData.historicalData
+            let data = TestingData.historicalData
             
             let url = try XCTUnwrap(URL(string: "https://www.apple.com"))
-            stubRateSession.urlResponse = HTTPURLResponse(url: url,
-                                                              statusCode: 200,
-                                                              httpVersion: nil,
-                                                              headerFields: nil)
-            stubRateSession.error = nil
+            let response = HTTPURLResponse(url: url,
+                                           statusCode: 200,
+                                           httpVersion: nil,
+                                           headerFields: nil)
+            stubRateSession.outputs = [(data: data, response: response, error: nil)]
         }
         
         // action
@@ -81,25 +89,61 @@ final class FetcherTests: XCTestCase {
                     
                     let dummyCurrency = Currency.TWD
                     XCTAssertNotNil(rate[dummyCurrency])
+                    
+                    expectation.fulfill()
                 case .failure:
-                    XCTFail()
+                    XCTFail("should get a decoded historical rate instance")
                 }
             }
+        
+        waitForExpectations(timeout: timeoutTimeInterval)
+    }
+    
+    func testDecodeFail() throws {
+        // arrange
+        let expectation = expectation(description: "should fail to decode")
+        let dummyEndpoint = Endpoint.Latest()
+        do {
+            let data = Data()
+            
+            let url = try XCTUnwrap(URL(string: "https://www.apple.com"))
+            let response = HTTPURLResponse(url: url,
+                                           statusCode: 204,
+                                           httpVersion: nil,
+                                           headerFields: nil)
+            stubRateSession.outputs = [(data: data, response: response, error: nil)]
+        }
+        
+        // action
+        sut
+            .fetch(dummyEndpoint) { result in
+                switch result {
+                case .success:
+                    XCTFail("should fail to decode")
+                case .failure(let failure):
+                    if failure is DecodingError {
+                        expectation.fulfill()
+                    } else {
+                        XCTFail("get an error other than decoding error: \(failure)")
+                    }
+                }
+            }
+        
+        waitForExpectations(timeout: timeoutTimeInterval)
     }
 }
 
-private class StubRateSession: RateSession {
+private final class StubRateSession: RateSession {
     
-    var data: Data?
-    
-    var urlResponse: URLResponse?
-    
-    var error: Error?
+    var outputs: [(data: Data?, response: URLResponse?, error: Error?)] = []
     
     func rateDataTask(
         with request: URLRequest,
         completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
     ) {
-        completionHandler(TestingData.historicalData, nil, nil)
+        guard !(outputs.isEmpty) else { return }
+        
+        let output = outputs.removeFirst()
+        completionHandler(output.data, output.response, output.error)
     }
 }
--- Supporting Files/Git Commit Info.swift
+++ Supporting Files/Git Commit Info.swift
@@ -1,3 +1,3 @@
 // 這個檔案是用 run script 產生的
-let gitHash = "ed2c9b0"
-let commitTimestamp = 1678605620
+let gitHash = "48bb13c"
+let commitTimestamp = 1678606294
