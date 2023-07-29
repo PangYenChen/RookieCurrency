18464bb12a893306e65ac88e942e8997982b58db
PangYen Chen <cpy4869@gmail.com>
2023-07-29 20:56:54 +0800

    [refine] remove unused file (this project does not localize storyboard)

--- Supporting Files/Git Commit Info.swift
+++ Supporting Files/Git Commit Info.swift
@@ -1,5 +1,5 @@
 // 這個檔案是用 run script 產生的
 extension AppUtility {
-    static let gitHash = "7822416"
-    static let commitTimestamp = 1690620463
+    static let gitHash = "972e2cf"
+    static let commitTimestamp = 1690634888
 }
