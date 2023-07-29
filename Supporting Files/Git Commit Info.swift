c7660ea66181db5b33d418ddd986e0c5c89990eb
PangYen Chen <cpy4869@gmail.com>
2023-07-29 16:27:26 +0800

    [refine] rename scheme

--- Supporting Files/Git Commit Info.swift
+++ Supporting Files/Git Commit Info.swift
@@ -1,5 +1,5 @@
 // 這個檔案是用 run script 產生的
 extension AppUtility {
-    static let gitHash = "0df4071"
-    static let commitTimestamp = 1690617631
+    static let gitHash = "599a8ed"
+    static let commitTimestamp = 1690618381
 }
