78224168bbe9b2a88a50bce2b48d8b42b60e10eb
PangYen Chen <cpy4869@gmail.com>
2023-07-29 16:47:43 +0800

    [refine] edit xpbproj directly

--- Supporting Files/Git Commit Info.swift
+++ Supporting Files/Git Commit Info.swift
@@ -1,5 +1,5 @@
 // 這個檔案是用 run script 產生的
 extension AppUtility {
-    static let gitHash = "81ff2c1"
-    static let commitTimestamp = 1690619386
+    static let gitHash = "4b37c4b"
+    static let commitTimestamp = 1690619638
 }
