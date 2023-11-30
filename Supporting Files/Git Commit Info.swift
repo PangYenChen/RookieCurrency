06b2ad5db574dc632c86ca835bdcb81b6285db35
ChenPangYen <cpy4869@gmail.com>
2023-11-30 19:33:03 +0800

    [lint] 更新 lint 規則

--- Supporting Files/Git Commit Info.swift
+++ Supporting Files/Git Commit Info.swift
@@ -1,5 +1,5 @@
 // 這個檔案是用 run script 產生的
 extension AppUtility {
-    static let gitHash = "45be0ab"
-    static let commitTimestamp = 1701329232
+    static let gitHash = "2a83d86"
+    static let commitTimestamp = 1701339234
 }
