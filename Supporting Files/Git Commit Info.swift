253f34e52b42b3da2410157beda3e6704151433a
PangYen Chen <cpy4869@gmail.com>
2024-01-24 21:27:50 +0800

    [feature] complete swift strict concurrency

--- Supporting Files/Git Commit Info.swift
+++ Supporting Files/Git Commit Info.swift
@@ -1,5 +1,5 @@
 // 這個檔案是用 run script 產生的
 extension AppUtility {
-    static let gitHash = "96a036e"
-    static let commitTimestamp = 1705497647
+    static let gitHash = "99aa4b1"
+    static let commitTimestamp = 1706703461
 }
