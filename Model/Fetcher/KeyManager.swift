e57d44b9dde9ee42e7f60b1966502d983b42258b
PangYen Chen <cpy4869@gmail.com>
2024-04-04 15:29:29 -0400

    add key manager

---
+++ Model/Fetcher/KeyManager.swift
@@ -0,0 +1,70 @@
+import Foundation
+
+class KeyManager {
+    init(concurrentQueue: DispatchQueue = DispatchQueue(label: "key.manager", attributes: .concurrent),
+         unusedAPIKeys: Set<String> = KeyManager.unusedAPIKeys) {
+        self.concurrentQueue = concurrentQueue
+        
+        self.unusedAPIKeys = unusedAPIKeys
+        
+        if let unusedAPIKey = self.unusedAPIKeys.popFirst() {
+            usingAPIKeyResult = .success(unusedAPIKey)
+        }
+        else {
+            usingAPIKeyResult = .failure(Error.runOutOfKey)
+        }
+        
+        self.usedAPIKeys = []
+    }
+    
+    private let concurrentQueue: DispatchQueue
+    
+    private var unusedAPIKeys: Set<String>
+    private var usingAPIKeyResult: Result<String, Error>
+    private var usedAPIKeys: Set<String> = []
+    
+#if DEBUG
+    var apiKeysUsageRatio: Double {
+        let totalAPIKeyCount: Int = usedAPIKeys.count + 1 + unusedAPIKeys.count
+        return Double(usedAPIKeys.count) / Double(totalAPIKeyCount)
+    }
+#endif
+}
+
+extension KeyManager {
+    func getUsingAPIKeyAfterDeprecating(_ apiKeyToBeDeprecated: String) -> Result<String, Error> {
+        concurrentQueue.sync(flags: .barrier) {
+            guard case let .success(usingAPIKey) = usingAPIKeyResult else { return .failure(.runOutOfKey) }
+            guard apiKeyToBeDeprecated == usingAPIKey else { return .success(usingAPIKey) }
+            
+            if let unusedAPIKey = unusedAPIKeys.popFirst() {
+                usedAPIKeys.insert(apiKeyToBeDeprecated)
+                usingAPIKeyResult = .success(unusedAPIKey)
+            }
+            else {
+                usingAPIKeyResult = .failure(.runOutOfKey)
+            }
+            
+            return usingAPIKeyResult
+        }
+    }
+    
+    func getUsingAPIKey() -> Result<String, Error> {
+        concurrentQueue.sync { usingAPIKeyResult }
+    }
+}
+
+extension KeyManager {
+    private static let unusedAPIKeys: Set<String> = ["kGm2uNHWxJ8WeubiGjTFhOG1uKs3iVsW",
+                                                     "pT4L8AtpKOIWiGoE0ouiak003mdE0Wvg",
+                                                     "R7fbgnoWFqhDtzxrfbYNgTbRJqLcNplL",
+                                                     "02aTmnyTuDcfXXdo4AruyhfsW3vDVCD4"]
+    
+    static let shared: KeyManager = KeyManager()
+}
+
+extension KeyManager {
+    enum Error: LocalizedError {
+        case runOutOfKey
+    }
+}
--- Supporting Files/Git Commit Info.swift
+++ Supporting Files/Git Commit Info.swift
@@ -1,5 +1,5 @@
 // 這個檔案是用 run script 產生的
 extension AppUtility {
-    static let gitHash: String = "9a96ee9"
-    static let commitTimestamp: Int = 1712178828
+    static let gitHash: String = "5acf798"
+    static let commitTimestamp: Int = 1712195662
 }
