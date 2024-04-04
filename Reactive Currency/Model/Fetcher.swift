e1356f5c75c227b37a12388ca1467c27f20da6dd
PangYen Chen <cpy4869@gmail.com>
2024-04-04 16:16:06 -0400

    reactive, use key manager

--- Reactive Currency/Model/Fetcher.swift
+++ Reactive Currency/Model/Fetcher.swift
@@ -12,52 +12,56 @@ class Fetcher: BaseFetcher, FetcherProtocol {
     /// - Returns: The publisher publishes decoded instance when the task completes, or terminates if the task fails with an error.
     func publisher<Endpoint: EndpointProtocol>(for endpoint: Endpoint) -> AnyPublisher<Endpoint.ResponseType, Swift.Error> {
         func dataTaskPublisherWithLimitHandling(for endpoint: Endpoint) -> AnyPublisher<(data: Data, response: URLResponse), Swift.Error> {
-            let apiKey: String = getUsingAPIKey()
-            
-            return rateSession.rateDataTaskPublisher(for: createRequest(url: endpoint.url, withAPIKey: apiKey))
-                .mapError { $0 }
-                .flatMap { [unowned self] data, response -> AnyPublisher<(data: Data, response: URLResponse), Swift.Error> in
-                    if let httpURLResponse = response as? HTTPURLResponse {
-                        if httpURLResponse.statusCode == 401 {
-                            // server 回應 status code 401，表示 api key 無效
-                            if updateAPIKeySucceed(apiKeyToBeDeprecated: apiKey) {
-                                // 更新完 api key 後重新打 api
-                                return dataTaskPublisherWithLimitHandling(for: endpoint)
-                                    .eraseToAnyPublisher()
+            switch keyManager.getUsingAPIKey() {
+                case .success(let apiKey):
+                    return rateSession.rateDataTaskPublisher(for: createRequest(url: endpoint.url, withAPIKey: apiKey))
+                        .mapError { $0 }
+                        .flatMap { [unowned self] data, response -> AnyPublisher<(data: Data, response: URLResponse), Swift.Error> in
+                            if let httpURLResponse = response as? HTTPURLResponse {
+                                if httpURLResponse.statusCode == 401 {
+                                    // server 回應 status code 401，表示 api key 無效
+                                    switch keyManager.getUsingAPIKeyAfterDeprecating(apiKey) {
+                                        case .success:
+                                            // 更新完 api key 後重新打 api
+                                            return dataTaskPublisherWithLimitHandling(for: endpoint)
+                                                .eraseToAnyPublisher()
+                                        case .failure:
+                                            // 沒有有效 api key 可用
+                                            return Fail(error: Fetcher.Error.invalidAPIKey)
+                                                .eraseToAnyPublisher()
+                                    }
+                                }
+                                else if httpURLResponse.statusCode == 429 {
+                                    // server 回應 status code 429，表示 api key 額度用完
+                                    switch keyManager.getUsingAPIKeyAfterDeprecating(apiKey) {
+                                        case .success:
+                                            // 更新完 api key 後重新打 api
+                                            return dataTaskPublisherWithLimitHandling(for: endpoint)
+                                                .eraseToAnyPublisher()
+                                        case .failure:
+                                            // 已經沒有還有額度的 api key 可以用了
+                                            return Fail(error: Fetcher.Error.tooManyRequest)
+                                                .eraseToAnyPublisher()
+                                    }
+                                }
+                                else {
+                                    // 這是一切都正常的情況，把 data 跟 response 往下傳
+                                    return Just((data: data, response: response))
+                                        .setFailureType(to: Swift.Error.self)
+                                        .eraseToAnyPublisher()
+                                }
                             }
                             else {
-                                // 沒有有效 api key 可用
-                                return Fail(error: Fetcher.Error.invalidAPIKey)
+                                assertionFailure("###, \(#function), \(self), response 不是 HttpURLResponse，常理來說都不會發生。")
+                                return Fail(error: Error.unknownError)
                                     .eraseToAnyPublisher()
                             }
                         }
-                        else if httpURLResponse.statusCode == 429 {
-                            // server 回應 status code 429，表示 api key 額度用完
-                            if updateAPIKeySucceed(apiKeyToBeDeprecated: apiKey) {
-                                // 更新完 api key 後重新打 api
-                                return dataTaskPublisherWithLimitHandling(for: endpoint)
-                                    .eraseToAnyPublisher()
-                            }
-                            else {
-                                // 已經沒有還有額度的 api key 可以用了
-                                return Fail(error: Fetcher.Error.tooManyRequest)
-                                    .eraseToAnyPublisher()
-                            }
-                        }
-                        else {
-                            // 這是一切都正常的情況，把 data 跟 response 往下傳
-                            return Just((data: data, response: response))
-                                .setFailureType(to: Swift.Error.self)
-                                .eraseToAnyPublisher()
-                        }
-                    }
-                    else {
-                        assertionFailure("###, \(#function), \(self), response 不是 HttpURLResponse，常理來說都不會發生。")
-                        return Fail(error: Error.unknownError)
-                            .eraseToAnyPublisher()
-                    }
-                }
-                .eraseToAnyPublisher()
+                        .eraseToAnyPublisher()
+                case .failure(let failure):
+                    return Fail(error: Error.invalidAPIKey)
+                        .eraseToAnyPublisher()
+            }
         }
         
         return dataTaskPublisherWithLimitHandling(for: endpoint)
--- Supporting Files/Git Commit Info.swift
+++ Supporting Files/Git Commit Info.swift
@@ -1,5 +1,5 @@
 // 這個檔案是用 run script 產生的
 extension AppUtility {
-    static let gitHash: String = "e57d44b"
-    static let commitTimestamp: Int = 1712258969
+    static let gitHash: String = "2d9fc0c"
+    static let commitTimestamp: Int = 1712261156
 }
