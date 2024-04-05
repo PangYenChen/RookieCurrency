6e016ebf68eb6dc9b899fc1d91430d6c02f7df6d
PangYen Chen <cpy4869@gmail.com>
2024-04-05 15:04:03 -0400

    [test] testRunOutOfKeysConcurrently

--- Shared Tests/KeyManagerTests.swift
+++ Shared Tests/KeyManagerTests.swift
@@ -13,7 +13,7 @@ final class KeyManagerTests: XCTestCase {
     
     private var concurrentQueue: DispatchQueue!
     private var unusedAPIKeys: Set<String>!
-
+    
     override func setUp() {
         concurrentQueue = DispatchQueue(label: "key.manager.tests", attributes: .concurrent)
         unusedAPIKeys = Set((0..<100).map { _ in UUID().uuidString })
@@ -21,7 +21,7 @@ final class KeyManagerTests: XCTestCase {
         sut = KeyManager(concurrentQueue: concurrentQueue,
                          unusedAPIKeys: unusedAPIKeys)
     }
-
+    
     override func tearDown() {
         sut = nil
         
@@ -42,5 +42,27 @@ final class KeyManagerTests: XCTestCase {
         }
         
         XCTAssertNil(try? sut.getUsingAPIKeyAfterDeprecating(usingAPIKey).get())
+    }
+    
+    func testRunOutOfKeysConcurrently() throws {
+        // arrange
+        let concurrentQueue: DispatchQueue = DispatchQueue(label: "test.run.out.of.keys.concurrently", attributes: .concurrent)
+        
+        for _ in 0..<(unusedAPIKeys.count - 1) {
+            concurrentQueue.async { [unowned self] in
+                switch sut.getUsingAPIKey() {
+                    case .success(let usingAPIKey):
+                        switch sut.getUsingAPIKeyAfterDeprecating(usingAPIKey) {
+                            case .success: break
+                            case .failure: XCTFail("api key的數量應該夠多，即使拿到的對象不對，至少能拿到東西")
+                        }
+                        
+                    case .failure:
+                        XCTFail("api key的數量應該夠多，即使拿到的對象不對，至少能拿到東西")
+                }
+            }
+        }
+        
+        concurrentQueue.sync(flags: .barrier) { /*intentionally left blank*/ }
     }
 }
--- Supporting Files/Git Commit Info.swift
+++ Supporting Files/Git Commit Info.swift
@@ -1,5 +1,5 @@
 // 這個檔案是用 run script 產生的
 extension AppUtility {
-    static let gitHash: String = "e1356f5"
-    static let commitTimestamp: Int = 1712261766
+    static let gitHash: String = "bc59ca0"
+    static let commitTimestamp: Int = 1712342931
 }
