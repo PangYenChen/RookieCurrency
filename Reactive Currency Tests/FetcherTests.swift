0454d9fa241a2a112cc218d688fa28066e59b563
PangYen Chen <cpy4869@gmail.com>
2024-04-06 21:13:46 -0400

    reactive testInvalidAPIKeyFallBack

--- Reactive Currency Tests/FetcherTests.swift
+++ Reactive Currency Tests/FetcherTests.swift
@@ -391,258 +391,57 @@ class FetcherTests: XCTestCase {
         
         XCTAssertEqual(keyManager.usedAPIKeys.count, 1)
     }
-//    
-//    /// session 回應 api key 無效（可能是我在服務商平台更新某個 api key），
-//    /// fetcher 更換新的 api key 後再次 call session 的 method，
-//    /// 後續的 api key 全都無效，fetcher 能回傳 api key 無效的 error。
-//    func testInvalidAPIKeyFallBack() throws {
-//        // arrange
-//        var receivedValue: ResponseDataModel.LatestRate?
-//        var receivedCompletion: Subscribers.Completion<Error>?
-//        
-//        let dummyEndpoint: Endpoints.Latest = Endpoints.Latest()
-//        
-//        do {
-//            stubRateSession.outputPublisher = try sessionDataPublisher(TestingData.SessionData.invalidAPIKey())
-//        }
-//        
-//        // act
-//        sut
-//            .publisher(for: dummyEndpoint)
-//            .sink(
-//                receiveCompletion: { completion in receivedCompletion = completion },
-//                receiveValue: { value in receivedValue = value }
-//            )
-//            .store(in: &anyCancellableSet)
-//        
-//        // assert
-//        do {
-//            let receivedCompletion: Subscribers.Completion<Error> = try XCTUnwrap(receivedCompletion)
-//            
-//            switch receivedCompletion {
-//                case .failure(let error):
-//                    guard let fetcherError = error as? Fetcher.Error else {
-//                        XCTFail("should receive Fetcher.Error")
-//                        return
-//                    }
-//                    
-//                    guard fetcherError == Fetcher.Error.invalidAPIKey else {
-//                        XCTFail("receive error other than Fetcher.Error.runOutOfQuota: \(error)")
-//                        return
-//                    }
-//                case .finished:
-//                    XCTFail("should not complete normally")
-//            }
-//        }
-//        
-//        do {
-//            XCTAssertNil(receivedValue)
-//        }
-//    }
-//    
-//
-//    
-//    /// 同時 call 兩次 session 的 method，
-//    /// 都回應 api key 的額度用罄，
-//    /// fetcher 要只更新 api key 一次。
-//    func testRunOutOfQuotaSimultaneously() throws {
-//        // arrange
-//        let spyAPIKeySession: SpyAPIKeyRateSession = SpyAPIKeyRateSession()
-//        sut = Fetcher(rateSession: spyAPIKeySession)
-//        
-//        let dummyEndpoint: Endpoints.Latest = Endpoints.Latest()
-//        
-//        var firstReceivedValue: ResponseDataModel.LatestRate?
-//        var firstReceivedCompletion: Subscribers.Completion<Error>?
-//        
-//        var secondReceivedValue: ResponseDataModel.LatestRate?
-//        var secondReceivedCompletion: Subscribers.Completion<Error>?
-//        
-//        // act
-//        sut.publisher(for: dummyEndpoint)
-//            .sink(
-//                receiveCompletion: { completion in firstReceivedCompletion = completion },
-//                receiveValue: { value in firstReceivedValue = value }
-//            )
-//            .store(in: &anyCancellableSet)
-//        
-//        sut.publisher(for: dummyEndpoint)
-//            .sink(
-//                receiveCompletion: { completion in secondReceivedCompletion = completion },
-//                receiveValue: { value in secondReceivedValue = value }
-//            )
-//            .store(in: &anyCancellableSet)
-//        
-//        do {
-//            let tooManyRequestTuple: (data: Data?, response: URLResponse?, error: Error?) = try XCTUnwrap(TestingData.SessionData.tooManyRequest())
-//            
-//            let data: Data = try XCTUnwrap(tooManyRequestTuple.data)
-//            let urlResponse: URLResponse = try XCTUnwrap(tooManyRequestTuple.response)
-//            
-//            // session publish 第一個 output
-//            if let firstOutputSubject = spyAPIKeySession.outputSubjects.first {
-//                firstOutputSubject.send((data, urlResponse))
-//                firstOutputSubject.send(completion: .finished)
-//            }
-//            else {
-//                XCTFail("arrange 失誤，第一次 subscribe `sut.publisher(for:)` 應該會給 subscribe spy api key session，進而產生一個 subject")
-//            }
-//            // session publish 第二個 output
-//            if spyAPIKeySession.outputSubjects.count >= 2 {
-//                let secondOutputSubject: PassthroughSubject<(data: Data, response: URLResponse), URLError> = spyAPIKeySession.outputSubjects[1]
-//                secondOutputSubject.send((data, urlResponse))
-//                secondOutputSubject.send(completion: .finished)
-//            }
-//            else {
-//                XCTFail("arrange 失誤，第二次 subscribe `sut.publisher(for:)` 應該會給 subscribe spy api key session，進而產生第二個 subject")
-//            }
-//            
-//            // 現階段 fetcher 應該還沒 publish 任何 output
-//            XCTAssertNil(firstReceivedValue)
-//            XCTAssertNil(secondReceivedValue)
-//        }
-//        
-//        do {
-//            let latestRateTuple: (data: Data?, response: URLResponse?, error: Error?) = try TestingData.SessionData.latestRate()
-//            
-//            let data: Data = try XCTUnwrap(latestRateTuple.data)
-//            let urlResponse: URLResponse = try XCTUnwrap(latestRateTuple.response)
-//            
-//            // session publish 第三個 output
-//            if spyAPIKeySession.outputSubjects.count >= 3 {
-//                let thirdOutputSubject: PassthroughSubject<(data: Data, response: URLResponse), URLError> = spyAPIKeySession.outputSubjects[2]
-//                thirdOutputSubject.send((data, urlResponse))
-//                thirdOutputSubject.send(completion: .finished)
-//            }
-//            else {
-//                XCTFail("arrange 失誤， spy api key session 針對 fetcher 第一次的 subscribe publish too many request 的 error，fetcher 換完 api key 後會重新 subscribe spy api key session，這時候應該要產生第三個 subject。")
-//            }
-//            
-//            // session publish 第四個 output
-//            if spyAPIKeySession.outputSubjects.count >= 4 {
-//                let fourthOutputSubject: PassthroughSubject<(data: Data, response: URLResponse), URLError> = spyAPIKeySession.outputSubjects[3]
-//                fourthOutputSubject.send((data, urlResponse))
-//                fourthOutputSubject.send(completion: .finished)
-//            }
-//            else {
-//                XCTFail("arrange 失誤， spy api key session 針對 fetcher 第二次的 subscribe publish too many request 的 error，fetcher 換完 api key 後會重新 subscribe spy api key session，這時候應該要產生第四個 subject。")
-//            }
-//        }
-//        
-//        // assert
-//        if spyAPIKeySession.receivedAPIKeys.count == 4 {
-//            XCTAssertEqual(spyAPIKeySession.receivedAPIKeys[0], spyAPIKeySession.receivedAPIKeys[1])
-//            XCTAssertEqual(spyAPIKeySession.receivedAPIKeys[2], spyAPIKeySession.receivedAPIKeys[3])
-//        }
-//        else {
-//            XCTFail("spy api key session 應該要剛好收到 4 個 request")
-//        }
-//        
-//        do {
-//            XCTAssertNotNil(firstReceivedValue)
-//            
-//            let firstReceivedCompletion: Subscribers.Completion<Error> = try XCTUnwrap(firstReceivedCompletion)
-//            switch firstReceivedCompletion {
-//                case .finished:
-//                    break
-//                case .failure(let failure):
-//                    XCTFail("不應該收到錯誤卻收到\(failure)")
-//            }
-//        }
-//        
-//        do {
-//            XCTAssertNotNil(secondReceivedValue)
-//            
-//            let secondReceivedCompletion: Subscribers.Completion<Error> = try XCTUnwrap(secondReceivedCompletion)
-//            switch secondReceivedCompletion {
-//                case .finished:
-//                    break
-//                case .failure(let failure):
-//                    XCTFail("不應該收到錯誤卻收到\(failure)")
-//            }
-//        }
-//    }
+    
+    /// session 回應 api key 無效（可能是我在服務商平台更新某個 api key），
+    /// fetcher 能通知 key manager，key manager 更新 key 之後
+    /// fetcher 重新打 api，
+    /// 後續的 api key 全都無效，fetcher 能回傳 api key 無效的 error。
+    func testInvalidAPIKeyFallBack() throws {
+        // arrange
+        var receivedValue: ResponseDataModel.TestDataModel?
+        var receivedCompletion: Subscribers.Completion<Error>?
+        
+        // act
+        do {
+            let dummyURL: URL = try XCTUnwrap(URL(string: "https://www.apple.com"))
+            sut
+                .publisher(for: Endpoints.TestEndpoint(url: dummyURL))
+                .sink(receiveCompletion: { completion in receivedCompletion = completion },
+                      receiveValue: { value in receivedValue = value })
+                .store(in: &anyCancellableSet)
+            do /*session result in invalid api key*/ {
+                for _ in 0..<dummyAPIKeys.count {
+                    let tuple: (data: Data?, response: URLResponse?, error: Error?) = try TestingData
+                        .CurrencySessionTuple
+                        .invalidAPIKey()
+                    try currencySession.publish((XCTUnwrap(tuple.data),
+                                                 XCTUnwrap(tuple.response)))
+                }
+            }
+        }
+        
+        // assert
+        XCTAssertNil(receivedValue)
+        
+        do /*assert receivedCompletion*/ {
+            let receivedCompletion: Subscribers.Completion<Error> = try XCTUnwrap(receivedCompletion)
+            
+            switch receivedCompletion {
+                case .failure(let error):
+                    guard let fetcherError = error as? Fetcher.Error else {
+                        XCTFail("should receive Fetcher.Error")
+                        return
+                    }
+                    
+                    guard fetcherError == Fetcher.Error.invalidAPIKey else {
+                        XCTFail("receive error other than Fetcher.Error.runOutOfQuota: \(error)")
+                        return
+                    }
+                case .finished:
+                    XCTFail("should not complete normally")
+            }
+        }
+        
+        XCTAssertEqual(keyManager.usedAPIKeys.count, dummyAPIKeys.count)
+    }
 }
-//
-//// MARK: - private helper method
-//private extension FetcherTests {
-//    func sessionDataPublisher(_ tuple: (data: Data?, response: URLResponse?, error: Error?)) throws -> AnyPublisher<(data: Data, response: URLResponse), URLError> {
-//        if let urlError = tuple.error as? URLError {
-//            return Fail(error: urlError)
-//                .eraseToAnyPublisher()
-//        }
-//        else {
-//            let data: Data = try XCTUnwrap(tuple.data)
-//            let urlResponse: URLResponse = try XCTUnwrap(tuple.response)
-//            
-//            return Just((data: data, response: urlResponse))
-//                .setFailureType(to: URLError.self)
-//                .eraseToAnyPublisher()
-//        }
-//    }
-//}
-//
-//// MARK: - name space: test double
-//private extension FetcherTests {
-//    private class StubRateSession: CurrencySessionProtocol {
-//        var outputPublisher: AnyPublisher<(data: Data, response: URLResponse), URLError>!
-//        
-//        func rateDataTaskPublisher(for request: URLRequest) -> AnyPublisher<(data: Data, response: URLResponse), URLError> {
-//            outputPublisher
-//        }
-//    }
-//    
-//    class SpyRateSession: CurrencySessionProtocol {
-//        // MARK: - initializer
-//        init() {
-//            receivedAPIKeys = []
-//            outputPublishers = []
-//        }
-//        
-//        // MARK: - instance properties
-//        private(set) var receivedAPIKeys: [String]
-//        
-//        var outputPublishers: [AnyPublisher<(data: Data, response: URLResponse), URLError>]
-//        
-//        // MARK: - instance method
-//        func rateDataTaskPublisher(for request: URLRequest) -> AnyPublisher<(data: Data, response: URLResponse), URLError> {
-//            if let apiKey = request.value(forHTTPHeaderField: "apikey") {
-//                receivedAPIKeys.append(apiKey)
-//            }
-//            
-//            if outputPublishers.isEmpty {
-//                return Empty()
-//                    .setFailureType(to: URLError.self)
-//                    .eraseToAnyPublisher()
-//            }
-//            else {
-//                return outputPublishers.removeFirst()
-//            }
-//        }
-//    }
-//    
-//    final class SpyAPIKeyRateSession: CurrencySessionProtocol {
-//        // MARK: - initializer
-//        init() {
-//            receivedAPIKeys = []
-//            outputSubjects = []
-//        }
-//        
-//        // MARK: - instance properties
-//        private(set) var receivedAPIKeys: [String]
-//        
-//        private(set) var outputSubjects: [PassthroughSubject<(data: Data, response: URLResponse), URLError>]
-//        
-//        // MARK: - instance method
-//        func rateDataTaskPublisher(for request: URLRequest) -> AnyPublisher<(data: Data, response: URLResponse), URLError> {
-//            if let receivedAPIKey = request.value(forHTTPHeaderField: "apikey") {
-//                receivedAPIKeys.append(receivedAPIKey)
-//            }
-//            
-//            let outputSubject: PassthroughSubject<(data: Data, response: URLResponse), URLError> = PassthroughSubject<(data: Data, response: URLResponse), URLError>()
-//            outputSubjects.append(outputSubject)
-//            
-//            return outputSubject.eraseToAnyPublisher()
-//        }
-//    }
-//}
--- Supporting Files/Git Commit Info.swift
+++ Supporting Files/Git Commit Info.swift
@@ -1,5 +1,5 @@
 // 這個檔案是用 run script 產生的
 extension AppUtility {
-    static let gitHash: String = "76baa32"
-    static let commitTimestamp: Int = 1712450166
+    static let gitHash: String = "a2ebdc3"
+    static let commitTimestamp: Int = 1712452251
 }
