4b4dde05cb31327877301b1aad9e6ca2d0a0f68f
PangYen Chen <cpy4869@gmail.com>
2024-05-28 19:45:36 +0800

    [doc] document swiftlint.yml

--- Rookie Currency/Supporting Files/Git Commit Info.swift
+++ Rookie Currency/Supporting Files/Git Commit Info.swift
@@ -1,5 +1,5 @@
 // 這個檔案是用 run script 產生的
 extension AppUtility {
-    static let gitHash: String = "f32ef7f"
-    static let commitTimestamp: Int = 1716895512
+    static let gitHash: String = "e763b44"
+    static let commitTimestamp: Int = 1716895759
 }
